1593a1d5174e9704e7b0f8a9a5abf3adc28beaa9
NkBe <HSSkyBoy@outlook.com>
2025-08-23 14:30:36 +0800

    fix com.neko.R

--- app/src/main/java/com/neko/splash/SplashActivity.java
+++ app/src/main/java/com/neko/splash/SplashActivity.java
@@ -14,7 +14,7 @@ import android.widget.ImageView;
 import androidx.annotation.NonNull;
 import io.nekohasekai.sagernet.ui.MainActivity;
 import io.nekohasekai.sagernet.ui.ThemedActivity;
-import com.neko.R;
+import io.nekohasekai.sagernet.R;
 
 public class SplashActivity extends ThemedActivity {
 
